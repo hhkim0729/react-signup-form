d043f083a86ff1f7f9c9e98403da6758d392a0b6
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-29 23:06:13 +0900

    Add Welcome component (#10)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
 import Signup from './components/Signup';
+import Welcome from './components/Welcome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Main />
     <Signup />
+    <Welcome />
   </React.StrictMode>
 );
