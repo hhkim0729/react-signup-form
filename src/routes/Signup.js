140c778c830a4c74d13cb620bf367155d12cc23a
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 21:00:47 +0900

    Add checking password (#4)

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -56,11 +56,14 @@ const Signup = ({ users, addUser }) => {
     const regEmail =
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
     const regPhone = /^[0-9]{2,3}[0-9]{3,4}[0-9]{4}/;
+    const regPassword = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}/;
     const regUsername = /^([a-zA-Z0-9_.]){2,10}$/;
     if (id === 'email') {
       newCheckList.email = regEmail.test(value);
     } else if (id === 'phone') {
       newCheckList.phone = regPhone.test(value);
+    } else if (id === 'password') {
+      newCheckList.password = regPassword.test(value);
     } else if (id === 'username') {
       newCheckList.username = regUsername.test(value);
     } else if (id === 'referral') {
@@ -127,6 +130,11 @@ const Signup = ({ users, addUser }) => {
           label="Password"
           onChange={handleChange}
         />
+        {infos.password.length > 0 && !checkList.password && (
+          <span>
+            Invalid password (at least 1 lower case, 1 upper case, 1 number)
+          </span>
+        )}
         <InputBox
           infos={infos.confirm}
           type="password"
@@ -134,6 +142,9 @@ const Signup = ({ users, addUser }) => {
           label="Confirm Password"
           onChange={handleChange}
         />
+        {infos.confirm.length > 0 && !(infos.password === infos.confirm) && (
+          <span>Password mismatch</span>
+        )}
         <InputBox
           infos={infos.username}
           type="text"
