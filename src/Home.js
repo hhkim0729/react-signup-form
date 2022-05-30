4370a5b114159f8756ade576ee56c2723216e9d7
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:16:25 +0900

    Add Home component (#8)

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <header>
+        <h1>The Coolest website</h1>
+      </header>
+      <main>
+        <section>
+          <span>Thank you for visiting!</span>
+          <button>Be my guest</button>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './Main';
-import Signup from './components/Signup';
-import Welcome from './components/Welcome';
+import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
-    <Signup />
-    <Welcome />
+    <Home />
   </React.StrictMode>
 );
