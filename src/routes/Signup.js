18f778c8d904a167f0909e8aa1b75beabb27841a
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 16:01:29 +0900

    Refactor Signup component (#1)

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,57 +1,38 @@
 import React from 'react';
+import InputBox from '../components/InputBox';
 
 const Signup = () => {
   return (
-    <>
-      <header>
-        <h1>Be my guest!</h1>
-      </header>
-      <main>
-        <section>
-          <form>
-            <div>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" />
-            </div>
-            <div>
-              <label htmlFor="phone">Phone</label>
-              <input type="tel" id="phone" />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
-            </div>
-            <div>
-              <label htmlFor="confirm">Confirm Password</label>
-              <input type="password" id="confirm" />
-            </div>
-            <div>
-              <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
-            </div>
-            <div>
-              <label htmlFor="referral">Referral Username</label>
-              <input type="text" id="referral" />
-            </div>
-            <div>
-              <input type="checkbox" id="terms" />
-              <label htmlFor="terms">I agree to the Terms and Conditions</label>
-            </div>
-            <div>
-              <input type="checkbox" id="privacy" />
-              <label htmlFor="privacy">I agree to the Privacy Policy</label>
-            </div>
-            <div>
-              <input type="checkbox" id="marketing" />
-              <label htmlFor="marketing">
-                I agree to receive marketing emails
-              </label>
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
-      </main>
-    </>
+    <section>
+      <h1>Be my guest!</h1>
+      <form>
+        <InputBox type="email" id="email" label="Email" />
+        <InputBox type="tel" id="phone" label="Phone" />
+        <InputBox type="password" id="password" label="Password" />
+        <InputBox type="password" id="confirm" label="Confirm Password" />
+        <InputBox type="text" id="username" label="Username" />
+        <InputBox type="text" id="referral" label="Referral Username" />
+        <InputBox
+          type="checkbox"
+          id="terms"
+          label="I agree to the Terms and Conditions"
+          isLabelFirst={false}
+        />
+        <InputBox
+          type="checkbox"
+          id="privacy"
+          label="I agree to the Privacy Policy"
+          isLabelFirst={false}
+        />
+        <InputBox
+          type="checkbox"
+          id="marketing"
+          label="I agree to receive marketing emails"
+          isLabelFirst={false}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 };
 
