87e60a2e1fac2efc55fa8062b047427c7e0fec4b
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:52:32 +0900

    Make span h1 (#9)

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Main() {
   return (
     <section>
-      <span>Thank you for visiting!</span>
-      <button>Be my guest</button>
+      <h1>Thank you for visiting!</h1>
+      <Link to="signup">Be my guest</Link>
     </section>
   );
 }
