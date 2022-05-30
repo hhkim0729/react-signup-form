960e3bb3a6fdbee8afd1ac93f67c008e63e23c5a
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:53:01 +0900

    Simplify Welcome component (#10)

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -2,16 +2,9 @@ import React from 'react';
 
 const Welcome = () => {
   return (
-    <>
-      <header>
-        <h1>The Coolest website</h1>
-      </header>
-      <main>
-        <section>
-          <span>We're happy you're here, username!</span>
-        </section>
-      </main>
-    </>
+    <section>
+      <h1>We're happy you're here, username!</h1>
+    </section>
   );
 };
 
