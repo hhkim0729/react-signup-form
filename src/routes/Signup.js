2d0219352fb05c86839ea911a7eccd264d453985
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:33:08 +0900

    Correct for to htmlFor

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -10,40 +10,40 @@ const Signup = () => {
         <section>
           <form>
             <div>
-              <label for="email">Email</label>
+              <label htmlFor="email">Email</label>
               <input type="email" id="email" />
             </div>
             <div>
-              <label for="phone">Phone</label>
+              <label htmlFor="phone">Phone</label>
               <input type="tel" id="phone" />
             </div>
             <div>
-              <label for="password">Password</label>
+              <label htmlFor="password">Password</label>
               <input type="password" id="password" />
             </div>
             <div>
-              <label for="confirm">Confirm Password</label>
+              <label htmlFor="confirm">Confirm Password</label>
               <input type="password" id="confirm" />
             </div>
             <div>
-              <label for="username">Username</label>
+              <label htmlFor="username">Username</label>
               <input type="text" id="username" />
             </div>
             <div>
-              <label for="referral">Referral Username</label>
+              <label htmlFor="referral">Referral Username</label>
               <input type="text" id="referral" />
             </div>
             <div>
               <input type="checkbox" id="terms" />
-              <label for="terms">I agree to the Terms and Conditions</label>
+              <label htmlFor="terms">I agree to the Terms and Conditions</label>
             </div>
             <div>
               <input type="checkbox" id="privacy" />
-              <label for="privacy">I agree to the Privacy Policy</label>
+              <label htmlFor="privacy">I agree to the Privacy Policy</label>
             </div>
             <div>
               <input type="checkbox" id="terms" />
-              <label for="terms">I agree to receive marketing emails</label>
+              <label htmlFor="terms">I agree to receive marketing emails</label>
             </div>
             <button type="submit">Submit</button>
           </form>
