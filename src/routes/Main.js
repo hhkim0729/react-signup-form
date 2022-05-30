e855565c45901e192b0fabc2c8ba445c355c8a07
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:35:34 +0900

    Simplify Main component

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -2,17 +2,10 @@ import React from 'react';
 
 function Main() {
   return (
-    <>
-      <header>
-        <h1>The Coolest website</h1>
-      </header>
-      <main>
-        <section>
-          <span>Thank you for visiting!</span>
-          <button>Be my guest</button>
-        </section>
-      </main>
-    </>
+    <section>
+      <span>Thank you for visiting!</span>
+      <button>Be my guest</button>
+    </section>
   );
 }
 
