3c817be47fdd2873b64935776d4cb85b70e5ae3b
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 15:34:36 +0900

    Use BrwoserRouter (#8)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
