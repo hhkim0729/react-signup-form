102f5b9cf721ccec2a60ab27e70804e274fbb352
hhkim0729 <heehkim@student.42seoul.kr>
2022-05-30 16:02:42 +0900

    Add routers (#8)

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Main from './routes/Main';
+import Signup from './routes/Signup';
+import Welcome from './routes/Welcome';
+import Error from './components/Error';
 
 function Home() {
   return (
-    <>
+    <div>
       <header>
-        <h1>The Coolest website</h1>
+        <nav>
+          <Link to="/welcome">welcome</Link>
+        </nav>
+        <div>
+          <Link to="/">The Coolest website</Link>
+        </div>
       </header>
       <main>
-        <section>
-          <span>Thank you for visiting!</span>
-          <button>Be my guest</button>
-        </section>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="welcome" element={<Welcome />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
       </main>
-    </>
+    </div>
   );
 }
 
